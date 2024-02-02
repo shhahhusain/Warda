cf0640ff7a355053e6355e6c21102547acb0f2d9
Shah Hussain <95882307+shhahhussain@users.noreply.github.com>
2024-02-02 14:50:01 +0500

    inital

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "ZAMA SHA BAS WARDAAAAAAAAAA",
+  descriptionHeader:
+    "They say some people worth melting I melt for you WARDA",
+  buttonYes: "Yes",
+  buttonNo: "No",
+  titleModar: "Thank you !!",
+  descriptionModar:
+    "URO BA HUM DARLA AOGHAGAMA ISTRI BA HUM DARLA KOMA  KOR BA HUM ZA SAFA KOMA ROTAI BA HUM DARTA ACHOMA ROTAI BA HUM DARTA PAKHOMA KHER DE BAS DE PAKHLE CHAL NA RAZE BUT AHA BAM EZDA KAMA KA STA ZRA WE ",
+};
