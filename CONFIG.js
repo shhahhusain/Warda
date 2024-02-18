7e6f5bc3f41dca39694b286e405004da5c51856c
Shah Hussain <95882307+shhahhussain@users.noreply.github.com>
2024-02-18 22:21:50 +0500

    dua

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
   titleHeader: "DUA",
   descriptionHeader:
-    "They say some people worth melting for I melt for you every single time DUAAAAAAAAAAA whenever you talk my heart goes like 📈📈📈📈 i can listen to you forever your voice is lit I wanna talk to you so much ",
+    "I want you to be my full time friend i want to talk to you 24/7....PLEASE BE MY BESTFRIEND SO YOU DONT HAVE TO BE FRIEND WITH ANYONE ONLY ME kisi aor ke sat kuch b bat nahi bas ",
   buttonYes: "Yes",
   buttonNo: "No",
   titleModar: "DUA A REQUEST",
   descriptionModar:
-    "DUA all I desire is to hear your voice endlessly, for it's all I long for. Just your words, flowing like sweet melodies, enchanting my soul with every syllable. I want nothing more than to listen to you, as if every word you speak is a love song meant only for me.",
+    "They say some people worth melting for I melt for you every single time DUAAAAAAAAAAA whenever you talk my heart goes like 📈📈📈📈 i can listen to you forever your voice is lit I wanna talk to you so much.PLEASE DONT SAY NOOOOOOOOOOOOOOOOO",
 };
